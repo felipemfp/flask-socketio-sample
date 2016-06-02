802bcc3bf63d041424c43c774410518f3bb5f0d2
Felipe Pontes <felipemfpontes@gmail.com>
2016-06-01 23:17:53 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A sample that show how to use Flask and SocketIO
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, json
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'justasecretkeythatihavetochange'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api')
+def api():
+    query = dict(request.args)
+    socketio.emit('log', { 'data': str(query) }, broadcast=True)
+    return 'received'
+
+@socketio.on('connect')
+def on_connect():
+    payload = {'data': 'connected'}
+    emit('log', payload, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
